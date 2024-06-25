bbb522ccb67396a0f758b8f6ba53318d87f0e2ad
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-06-25 15:12:18 +0200

    Una conexión entre Database Class y SQL Server usando Entity FrameWork + Migrations ha sido establecido

---
+++ WebApplication1/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models; //import like method in python
+
+namespace WebApplication1.Controllers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //BASIC SYNTAX IN EVERY DATABASES
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } // Using this one-line code from Entity Framework will make a Table in SQL Server
+    }
+}
---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        // In this area we will be creating multiple properties... 
+
+        // The properties we want to add here in Category table (this is a database btw)
+        // are the columns properties of the table Category... 
+
+        // using prop and pressing tab twice we can make a customisable property 
+
+        // We have something called Data Annotation 
+
+        [Key] // The Entity Framework Core will know that the first one which is ID is our primary key
+        public int Id { get; set; } // This is our primary key 
+        [Required] // Another data annotation to add required to any of the properties NOT NULL in SQL  
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    
+    
+    }
+}
