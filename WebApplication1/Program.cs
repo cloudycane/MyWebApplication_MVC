bf34aa53d13f89308b0228d974dcb8ed91eb563d
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-07-28 22:33:59 +0200

    Añadiendo en nav la ruta de Register y Login

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -12,8 +12,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // adding a builder service
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitofWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -32,7 +32,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
